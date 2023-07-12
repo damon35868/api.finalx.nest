333aa1d8e5a27ecd0b4f91be6ce91a1798da8bc7
D.M <578328962@qq.com>
2023-07-12 11:14:21 +0800

    添加首页不校验token

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthPublic } from './auth/auth.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @AuthPublic()
   @Get()
   getHello(): string {
     return this.appService.getHello();
