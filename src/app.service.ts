0799ee47daa1d6779cdf933bd653497488d3050c
D.M <578328962@qq.com>
2023-07-12 11:03:18 +0800

    feat: 添加首页文本

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return '<h2>NEST INDEX</h2>';
+    return '<h2>Call me dad</h2>';
   }
 }
